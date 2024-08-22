05061b46e594603801c64109ae53cff806936e8c
sanjanabharath <sanjanabarath@gmail.com>
2024-08-22 22:16:07 +0530

    Added homepage

---
+++ web-based-wallet/src/components/HomePage.tsx
@@ -0,0 +1,12 @@
+import { IoWallet } from "react-icons/io5";
+
+const HomePage = () => {
+  return (
+    <div>
+        <IoWallet size={90}/>
+        <h2 className="text-4xl"></h2>
+    </div>
+  )
+}
+
+export default HomePage
